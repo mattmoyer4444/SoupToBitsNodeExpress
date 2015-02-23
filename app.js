a940d97232cd9c82656fd17e4d078ac802af12ff
Matt Moyer <moyer_m@yahoo.com>
2015-02-23 17:40:18 -0600

    Added web front end files

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ var express = require('express');
 var app = express();
 
 
-app.get('/', function(request, response) {
-    response.send('OK');
+//app.get('/', function(request, response) {
+//    response.send('OK');
+//});
+
+app.use(express.static('public'));
+
+app.get('/cities', function (request, response) {
+    var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+    response.json(cities);
 });
 
-
-
 module.exports = app;
+
+
