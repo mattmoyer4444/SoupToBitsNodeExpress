48d6c1bc0915ee1c0fa2852b3e0f7319a476cad4
Matt Moyer <moyer_m@yahoo.com>
2015-02-24 09:27:43 -0600

    Added Heroku Redis togo

--- app.js
+++ app.js
@@ -24,9 +24,10 @@ if (process.env.REDISTOGO_URL) {
 
 } else {
     var client = redis.createClient();
+    client.select((process.env.NODE_ENV || 'development').length);
 }
 
-client.select((process.env.NODE_ENV || 'development').length);
+
 // End Redis connection
 
 client.hset('cities', 'Lotopia', 'some description');
