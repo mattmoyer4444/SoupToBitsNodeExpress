0572e257dc953878f4647f44a7fe55596428237f
Matt Moyer <moyer_m@yahoo.com>
2015-02-24 10:18:50 -0600

    Added delete function

--- test.js
+++ test.js
@@ -72,6 +72,21 @@ describe('Creating new cities', function () {
             .send('name=Springfield&description=where+the+simpsons+live')
             .expect(/springfield/i, done);
     });
+});
 
+describe('Deleting cities', function () {
+    before(function() {
+        client.hset('cities', 'Banana', 'a tasty fruit');
+    });
+
+    after(function() {
+        client.flushdb();
+    });
+
+    it('Returns a 204 status code', function (done) {
+        request(app)
+            .delete('/cities/Banana')
+            .expect(204, done);
+    });
 });
 
